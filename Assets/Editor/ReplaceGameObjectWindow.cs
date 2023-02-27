83730d1c7aca3b55ff08fc2ef723327a6cb10179
Maryam Kamel <maryamsaeed_mkamel@outlook.com>
2023-02-27 15:25:55 +0000

    applying replacing on one game Object

--- Assets/Editor/ReplaceGameObjectWindow.cs
+++ Assets/Editor/ReplaceGameObjectWindow.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,7 +9,9 @@ public class ReplaceGameObjectWindow : EditorWindow
     [SerializeField]
     private VisualTreeAsset m_VisualTreeAsset = default;
 
-    private SerializedObject target;
+    private ObjectField replacingObject;
+    private ObjectField toBeReplacedObject;
+    private Button replaceButton;
 
     [MenuItem("Tools/ReplaceGameObjectWindow")]
     public static void ShowWindow()
@@ -21,12 +25,42 @@ public class ReplaceGameObjectWindow : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
         // Instantiate UXML
-        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(labelFromUXML);
+        VisualElement windowVisualTree = m_VisualTreeAsset.Instantiate();
+        root.Add(windowVisualTree);
+
+        replacingObject = root.Query<ObjectField>("ReplacingObjectField");
+        replacingObject.objectType = typeof(GameObject);
+
+        toBeReplacedObject = root.Query<ObjectField>("ObjectToBeReplacedField");
+        toBeReplacedObject.objectType = typeof(GameObject);
+
+        replaceButton = root.Query<Button>("ReplaceButton");
+        replaceButton.clicked += OnReplaceButtonClicked;
+    }
+
+    private void OnReplaceButtonClicked()
+    {
+        var replacingObje = (GameObject) replacingObject.value;
+        var tobeReplacedObj = (GameObject) toBeReplacedObject.value;
+        Replace(replacingObje, tobeReplacedObj);
+    }
+
+    private void Replace(GameObject replacingObj, GameObject tobereplacedObj)
+    {
+        try
+        {
+            var newObj = Instantiate(replacingObj);
+            var oldObj = tobereplacedObj;
+            newObj.transform.position = oldObj.transform.position;
+            newObj.transform.localScale = oldObj.transform.localScale;
+            newObj.transform.rotation = oldObj.transform.rotation;
+            toBeReplacedObject.value = newObj;
+            DestroyImmediate(oldObj);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
     }
 }
