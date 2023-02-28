6a175658b48320ae01cd068d8dc178f3ce379c06
Maryam Kamel <maryamsaeed_mkamel@outlook.com>
2023-02-28 15:17:28 +0000

    final working editor window

--- Assets/Editor/ReplaceGameObjectWindow.cs
+++ Assets/Editor/ReplaceGameObjectWindow.cs
@@ -5,6 +5,7 @@ using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 internal sealed class ReplaceGameObjectWindow : EditorWindow
 {
     [SerializeField]
@@ -13,7 +14,13 @@ internal sealed class ReplaceGameObjectWindow : EditorWindow
     private ObjectField replacingObject;
     private ObjectField toBeReplacedObject;
     private Button replaceButton;
+    private Button replaceObjectsButton;
     private HelpBox windowHelpBox;
+    private InspectorElement listInspector;
+    private ScriptableObject listObject;
+    private SerializedObject serializedListObject;
+
+    [SerializeField]
     public List<GameObject> toBeReplacedGameobjects;
 
     [MenuItem("Tools/ReplaceGameObjectWindow")]
@@ -43,12 +50,48 @@ internal sealed class ReplaceGameObjectWindow : EditorWindow
         toBeReplacedObject = root.Query<ObjectField>("ObjectToBeReplacedField");
         toBeReplacedObject.objectType = typeof(GameObject);
 
+        DrawListViaDefaultInspector();
+
         replaceButton = root.Query<Button>("ReplaceButton");
         replaceButton.clicked += OnReplaceButtonClicked;
+
+        replaceObjectsButton = root.Query<Button>("ReplaceGameObjectsButton");
+        replaceObjectsButton.clicked += OnReplaceObjectsButton;
 
         windowHelpBox = root.Query<HelpBox>("HelpBox");
         windowHelpBox.messageType = HelpBoxMessageType.Error;
         windowHelpBox.visible = false;
+
+    }
+
+    private void OnReplaceObjectsButton()
+    {
+        var replacingObje = (GameObject) replacingObject.value;
+        ObjectsList obj = (ObjectsList)listObject;
+        foreach (var item in obj.ObjectsToBeReplaced)
+        {
+            Replace(replacingObje, item);
+        }
+        obj.ObjectsToBeReplaced.Clear();
+        Debug.Log("Objects replaced successfully");
+    }
+
+    private void DrawListAsScriptableObject()
+    {
+        ScriptableObject listObject = CreateInstance(typeof(ObjectsList));
+        SerializedObject serializedListObject = new SerializedObject(listObject);
+        SerializedProperty sproperty = serializedListObject.FindProperty("test");
+        ObjectsListDrawerUIE drawer = new ObjectsListDrawerUIE();
+        var visualElement = drawer.CreatePropertyGUI(sproperty);
+        windowRoot.Add(visualElement);
+    }
+
+    private void DrawListViaDefaultInspector()
+    {
+        listObject = CreateInstance(typeof(ObjectsList));
+        serializedListObject = new SerializedObject(listObject);
+        listInspector = windowRoot.Query<InspectorElement>("ObjectsListInspector");
+        listInspector.Bind(serializedListObject);
     }
 
     private void OnReplaceButtonClicked()
@@ -58,6 +101,7 @@ internal sealed class ReplaceGameObjectWindow : EditorWindow
         var replacingObje = (GameObject) replacingObject.value;
         var tobeReplacedObj = (GameObject) toBeReplacedObject.value;
         Replace(replacingObje, tobeReplacedObj);
+        toBeReplacedObject.value = tobeReplacedObj;
     }
 
     private void Replace(GameObject replacingObj, GameObject tobereplacedObj)
@@ -69,7 +113,7 @@ internal sealed class ReplaceGameObjectWindow : EditorWindow
             newObj.transform.position = oldObj.transform.position;
             newObj.transform.localScale = oldObj.transform.localScale;
             newObj.transform.rotation = oldObj.transform.rotation;
-            toBeReplacedObject.value = newObj;
+            //toBeReplacedObject.value = newObj;
             DestroyImmediate(oldObj);
         }
         catch (Exception e)
@@ -88,6 +132,29 @@ internal sealed class ReplaceGameObjectWindow : EditorWindow
     private void OnSelectionChange()
     {
         HideHelpBox();
+    }
+}
+
+[Serializable]
+public class ObjectsList : ScriptableObject
+{
+    public List<GameObject> ObjectsToBeReplaced;
+}
+
+// IngredientDrawerUIE
+[CustomPropertyDrawer(typeof(ObjectsList))]
+public class ObjectsListDrawerUIE : PropertyDrawer
+{
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        // Create property container element.
+        var listContainer = new VisualElement();
+
+        var listField = new PropertyField(property.FindPropertyRelative("ObjectsToBeReplaced"));
+
+        listContainer.Add(listField);
+
+        return listContainer;
     }
 }
 
