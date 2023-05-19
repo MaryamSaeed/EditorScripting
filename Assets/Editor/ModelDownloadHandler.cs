29577790e3bf42aca58bc100626d3eefc1502183
Maryam Kamel <MaryamSaeed@users.noreply.github.com>
2023-05-19 13:39:11 +0100

    remove unused  using statements

--- Assets/Editor/ModelDownloadHandler.cs
+++ Assets/Editor/ModelDownloadHandler.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Microsoft.Extensions;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.IO;
 using UnityEditor;
-using System;
 
 public class ModelDownloadHandler
 {
